67cdd7bd1e87bdd53c96c68c491e186eba159d84
Simon 徐 <simon@sc.10086.cn.defaultbadlist>
2020-05-20 00:37:25 +0800

    test stream price

--- src/controllers/BinanceController.ts
+++ src/controllers/BinanceController.ts
@@ -3,6 +3,20 @@ import {  httpPost, httpGet } from "../decorators/HttpRoutes";
 import BinanceService from '../services/partners/BinanceService';
 
 export default class BinanceController {
+
+    @httpGet('/price/bid')
+    async bid(ctx: koa.Context){
+        const Binance = require('node-binance-api');
+        const binance = new Binance().options({
+            APIKEY: 'lR7PKoiFSubZqjdtokWDexSYA2JrPhvToZfUGlxLYpSWjfBwxNSfxFFOtzYuDT7E',
+            APISECRET: 'A1fqkdb9hNTlt1Q1rjD1Bs4SaRZlinJvQId4UhV9ggoWwbsjqs2Sh1Y97Fx5WyIt'
+        });
+        binance.websockets.bookTickers( 'BTCUSDT', (error:any, ticker:any)=>{
+            console.log(error);
+            console.log(ticker);
+        });
+        ctx.body = 'just test';
+    }
     @httpGet("/trade/start")
     async start(ctx: koa.Context){
         const service = new BinanceService(ctx.DBConnection);
